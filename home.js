ad94d76a9ac2a7fc8c83e8de2fb462276dbbc4ee
Arushi <arushis@seas.upenn.edu>
2023-04-02 21:15:20 -0400

    updated

--- home.js
+++ home.js
@@ -4,13 +4,21 @@ const heartIcon = document.getElementById("heart");
 const heartbreakIcon = document.getElementById("heartbreak");
 
 function dislikeMovie() {
+    document.getElementById('mvcontainer')
+            .style.display = "none";
     document.getElementById('heartbreak')
             .style.display = "block";
     document.getElementById('dislike');
     setTimeout(hideHeartbreak, 1000);
+    document.getElementById('lists')
+            .style.display = "block";
+    document.getElementById('lists-dropdown')
+            .style.display = "block";   
 }
 
 function likeMovie() {
+    document.getElementById('mvcontainer')
+            .style.display = "none";
     document.getElementById('heart')
             .style.display = "block";
     document.getElementById('like');
